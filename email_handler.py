45ebfdbb1c9663e517e19a6e03afd1f94ffe75ae
enricollen <nello.enrico@gmail.com>
2025-03-14 12:02:56 +0100

    restored confirmation email sent in BCC to the organized address

--- email_handler.py
+++ email_handler.py
@@ -30,7 +30,7 @@ class EmailHandler:
 
     def send_confirmation_email(self, recipient_email, form_data, file_ids):
         """
-        Invia una email di conferma dopo la registrazione al partecipante e all'organizzatore
+        Invia una email di conferma dopo la registrazione al partecipante e all'organizzatore (in BCC)
         """
         server = None
         try:
@@ -43,10 +43,11 @@ class EmailHandler:
             image_link = f"https://drive.google.com/file/d/{file_ids.get('image')}/view" if file_ids.get('image') else "#"
             presentation_link = f"https://drive.google.com/file/d/{file_ids.get('presentation')}/view" if file_ids.get('presentation') else "#"
 
-            # creo il messaggio con destinatari il candidato e l'organizzatore nel campo To
+            # creo il messaggio con l'organizzatore in BCC
             msg = MIMEMultipart()
             msg['From'] = f"Patient Engagement Award – Helaglobe"
-            msg['To'] = f"{recipient_email}, {self.organizer_email}"
+            msg['To'] = recipient_email
+            msg['Bcc'] = self.organizer_email
             msg['Subject'] = "Conferma registrazione al Patient Engagament Award 2025"
 
             # Corpo email
